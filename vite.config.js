4259837a3a1959255a6737835e9ae92c538b9c20
Kornel Chonre <kornel7139@gmail.com>
2025-06-24 12:21:25 +0600

    Initial Upload

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:"/LMS-Project/",
+  base:"/lms-project/",
   plugins: [react(), tailwindcss()],
 })
